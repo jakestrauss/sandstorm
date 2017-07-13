51b8c8f094b167934d255085630e064c4ff11103
jakestrauss <straussj@live.unc.edu>
2017-07-13 03:46:58 -0400

    Adding additional classes to git

---
+++ src/main/java/com/straussj/sandstorm/ExportProcessing.java
@@ -0,0 +1,118 @@
+package com.straussj.sandstorm;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.SettableFuture;
+import com.wrapper.spotify.Api;
+import com.wrapper.spotify.methods.AddTrackToPlaylistRequest;
+import com.wrapper.spotify.methods.CurrentUserRequest;
+import com.wrapper.spotify.methods.PlaylistCreationRequest;
+import com.wrapper.spotify.models.AuthorizationCodeCredentials;
+import com.wrapper.spotify.models.Playlist;
+import com.wrapper.spotify.models.Track;
+import com.wrapper.spotify.models.User;
+
+/**
+ * Servlet implementation class ExportProcessing
+ */
+public class ExportProcessing extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExportProcessing() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Application details necessary to get an access token */
+		final String code = request.getParameter("code");
+		String userID = "";
+		final Api api = (Api)request.getSession().getAttribute("api");
+		
+		/* Make a token request. Asynchronous requests are made with the .getAsync method and synchronous requests
+		 * are made with the .get method. This holds for all type of requests. */
+		final SettableFuture<AuthorizationCodeCredentials> authorizationCodeCredentialsFuture = api.authorizationCodeGrant(code).build().getAsync();
+
+		/* Add callbacks to handle success and failure */
+		Futures.addCallback(authorizationCodeCredentialsFuture, new FutureCallback<AuthorizationCodeCredentials>() {
+		  public void onSuccess(AuthorizationCodeCredentials authorizationCodeCredentials) {
+		    /* The tokens were retrieved successfully! */
+		    System.out.println("Successfully retrieved an access token! " + authorizationCodeCredentials.getAccessToken());
+		    System.out.println("The access token expires in " + authorizationCodeCredentials.getExpiresIn() + " seconds");
+		    System.out.println("Luckily, I can refresh it using this refresh token! " +     authorizationCodeCredentials.getRefreshToken());
+		  
+		    /* Set the access token and refresh token so that they are used whenever needed */
+		    api.setAccessToken(authorizationCodeCredentials.getAccessToken());
+		    api.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+		  }
+		  
+		  public void onFailure(Throwable throwable) {
+		    /* Let's say that the client id is invalid, or the code has been used more than once,
+		     * the request will fail. Why it fails is written in the throwable's message. */
+		  }
+		});
+		
+		final CurrentUserRequest currentuserrequest = api.getMe().build();
+		try {
+			final User user = currentuserrequest.get();
+			userID = user.getId();
+			System.out.println("Successfully retrieverd userID, which is " + userID);
+		} catch(Exception e) {
+			System.out.println("didn't get userID, error code is " + e.getMessage());
+		}
+		
+		//create new playlist for user
+		System.out.println("This is my userID " + userID);
+		final PlaylistCreationRequest PCrequest = api.createPlaylist(userID, "sandstorm").publicAccess(true).build();
+		String playlistID = "";
+		try {
+			final Playlist playlistCreated = PCrequest.get();
+			playlistID = playlistCreated.getId();
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		//getplay
+		
+		//add sandstorm songs to playlist
+		LocalPlaylist sandstorm = (LocalPlaylist)request.getSession().getAttribute("playlist");
+		List<Track> sandstormTracks = sandstorm.getTracks();
+		List<String> sandstormURIs = new ArrayList();
+		for(Track t: sandstormTracks) {
+			sandstormURIs.add(t.getUri());
+		}
+	
+		final AddTrackToPlaylistRequest aRequest = api.addTracksToPlaylist(userID, playlistID, sandstormURIs).build();
+		try {
+			aRequest.get();
+		} catch(Exception e) {
+			response.sendRedirect("Error.jsp");
+		}
+		
+		response.sendRedirect("ExportSuccess.jsp");
+	}
+
+}
---
+++ src/main/java/com/straussj/sandstorm/ExportToSpotify.java
@@ -0,0 +1,57 @@
+package com.straussj.sandstorm;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wrapper.spotify.Api;
+
+/**
+ * Servlet implementation class ExportToSpotify
+ */
+public class ExportToSpotify extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExportToSpotify() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Api api = Api.builder()
+				  .clientId("03f02ed981ec452aaaba403ae35cfca1")
+				  .clientSecret("5d74bd30f6094346ab30365a65605a02")
+				  .redirectURI("http://localhost:8080/sandstorm/callback.jsp")
+				  .build();
+		
+		/* Set the necessary scopes that the application will need from the user */
+		final List<String> scopes = Arrays.asList("user-modify-private", "playlist-modify-public", "user-read-private",
+				"user-read-email");
+
+		/* Set a state. This is used to prevent cross site request forgeries. */
+		final String state = "someExpectedStateString";
+		String authorizeURL = api.createAuthorizeURL(scopes, state);
+		request.getSession().setAttribute("api", api);
+		response.sendRedirect(authorizeURL);
+	}
+
+}
